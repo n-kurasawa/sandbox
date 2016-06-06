3ca74a06963824bef6b7b5389a3fe0bf6702b994
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2016-06-06 23:19:01 +0900

    tetris下準備

---
+++ tetris/index.js
@@ -0,0 +1,58 @@
+var camera, scene, renderer;
+var controls;
+
+init();
+render();
+
+function init() {
+  var container = document.createElement('div');
+  document.body.appendChild(container);
+
+  // scene
+  scene = new THREE.Scene();
+
+  // camera
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(200, 100, 300);
+  camera.lookAt(scene.position);
+
+  // mesh
+  var mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshLambertMaterial({color: 0x00ff80, overdraw: 0.5})
+  );
+  scene.add(mesh);
+
+  // light
+  // - 平行光
+  var light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 100, 30);
+  scene.add(light);
+  // - 自然光
+  var ambient = new THREE.AmbientLight(0x404040);
+  scene.add(ambient);
+
+  // renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xefefef);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  container.appendChild(renderer.domElement);
+
+  // controls
+  controls = new THREE.OrbitControls(camera);
+
+  // helper
+  var gridHelper = new THREE.GridHelper(200, 50);
+  var axisHlper = new THREE.AxisHelper(1000);
+  var lightHelper = new THREE.DirectionalLightHelper(light, 20);
+  scene.add(gridHelper);
+  scene.add(axisHlper);
+  scene.add(lightHelper);
+}
+
+function render() {
+  requestAnimationFrame(render);
+  controls.update();
+  renderer.render(scene, camera);
+}
