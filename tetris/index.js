fc971c549e2e332ede3c570f9f2b5b3eb497c869
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2016-06-08 23:02:32 +0900

    クリックの動き

--- tetris/index.js
+++ tetris/index.js
@@ -1,6 +1,7 @@
 var camera, scene, renderer;
 var controls;
 var block;
+var mouse = new THREE.Vector2(-2, -2);
 
 init();
 render();
@@ -17,43 +18,48 @@ function init() {
   camera.position.set(400, 800, 1300);
   camera.lookAt(scene.position);
 
-  // mesh
-  var mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(50, 50, 50),
-    new THREE.MeshLambertMaterial({color: 0x00ff80, overdraw: 0.5})
-  );
-  mesh.position.set(0, 0, 0);
-  scene.add(mesh);
-
   // block
-  block1 = new THREE.Mesh(
+  var block1 = new THREE.Mesh(
     new THREE.BoxGeometry(50, 50, 50),
     new THREE.MeshLambertMaterial({color: 0x0080ff, overdraw: 0.5})
   );
   block1.position.set(0, 50, 0);
-  block2 = new THREE.Mesh(
+  var block2 = new THREE.Mesh(
     new THREE.BoxGeometry(50, 50, 50),
     new THREE.MeshLambertMaterial({color: 0x0080ff, overdraw: 0.5})
   );
   block2.position.set(0, 0, 0);
-  block3 = new THREE.Mesh(
+  var block3 = new THREE.Mesh(
     new THREE.BoxGeometry(50, 50, 50),
     new THREE.MeshLambertMaterial({color: 0x0080ff, overdraw: 0.5})
   );
   block3.position.set(50, 0, 0);
-  block4 = new THREE.Mesh(
+  var block4 = new THREE.Mesh(
     new THREE.BoxGeometry(50, 50, 50),
     new THREE.MeshLambertMaterial({color: 0x0080ff, overdraw: 0.5})
   );
   block4.position.set(50, -50, 0);
 
   block = new THREE.Group();
-  block.position.y = 1000;
+  // block.position.y = 1000;
   block.add(block1);
   block.add(block2);
   block.add(block3);
   block.add(block4);
   scene.add(block);
+
+  scene.add(block1);
+  scene.add(block2);
+  scene.add(block3);
+  scene.add(block4);
+
+
+  var block5 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 50, 50),
+    new THREE.MeshLambertMaterial({color: 0x0080ff, overdraw: 0.5})
+  );
+  block5.position.set(150, 150, 0);
+  scene.add(block5);
 
   // light
   // - 平行光
@@ -74,6 +80,8 @@ function init() {
   // controls
   controls = new THREE.OrbitControls(camera);
 
+  document.addEventListener('click', onClick);
+
   // helper
   var gridHelper = new THREE.GridHelper(200, 50);
   var axisHlper = new THREE.AxisHelper(1000);
@@ -83,14 +91,31 @@ function init() {
   scene.add(lightHelper);
 }
 
+function onClick(e) {
+  // 1. マウス座標の取得
+  var rect = e.target.getBoundingClientRect();
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+
+  // 2. WebGLの座標系に変換
+  mouse.x = (e.clientX - rect.left) / window.innerWidth * 2 - 1;
+  mouse.y = -(e.clientY - rect.top) / window.innerHeight * 2 + 1;
+
+  var raycaster = new THREE.Raycaster();
+  var objs;
+
+  // 3. マウスから3D空間に光線を射出
+  raycaster.setFromCamera(mouse, camera);
+
+  // 4. 光線にあたった物体を取得、操作
+  objs = raycaster.intersectObjects(scene.children);
+  if (objs.length > 0) {
+    objs[0].object.material.emissive = new THREE.Color(0x999999);
+  }
+}
+
 function render() {
   requestAnimationFrame(render);
   controls.update();
-
-  // block.position.y -= 10;
-  if (block.position.y > 0) {
-    block.position.y -= 10;
-  }
-
   renderer.render(scene, camera);
 }
